5872a82a44011a8bf61da13a42270f7bc80a024d
vaibhavpan12 <vaibhavpan7558@gmail.com>
2024-07-25 10:57:25 +0530

    Update employee_management.sql

--- employee_management.sql
+++ employee_management.sql
@@ -1,14 +1,9 @@
 
--- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+
 --
 -- Host: localhost    Database: employee_management
 -- ------------------------------------------------------
--- Server version	8.0.26
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `employee_management`
@@ -30,15 +25,10 @@ CREATE TABLE `employees` (
   `position` varchar(255) NOT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 --
 -- Dumping data for table `employees`
 --
 
-/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
